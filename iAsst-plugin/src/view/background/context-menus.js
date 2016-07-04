58db506ef2ecb25369aa643868cab7e0154a1ce6
wxyz <hyhjwzx@126.com>
2016-07-04 22:51:34 +0800

    更新版本号并打包

--- iAsst-plugin/src/view/background/context-menus.js
+++ iAsst-plugin/src/view/background/context-menus.js
@@ -63,8 +63,7 @@ page.feild.selectionMenu = [{
  * 初始化整个整个菜单
  */
 page.event.init = function() {
-    var idx = 0;
-    var menuSize = page.feild.selectionMenu.length;
+
     for (var menuGroupIdx in page.feild.selectionMenu) {
         var menuGroup = page.feild.selectionMenu[menuGroupIdx];
 
@@ -126,22 +125,9 @@ page.func.openTab = function(url, args) {
     window.open(url, '_blank');
 };
 
-//function translate(info, tab) {
-//    var url = 'http://www.bing.com/translator/?h_text=msn_ctxt&setlang=zh-cn&from=%E8%87%AA%E5%8A%A8%E6%A3%80%E6%B5%8B&to=zh-CHS&text=' + info.selectionText;
-//    window.open(url, '_blank');
-//}
-
 function waitRead(info, tab) {
     alert(tab.title + '\n' + tab.url);
 }
-
-//chrome.contextMenus.create({
-//    type: 'normal',
-//    title: '使用Bing翻译',
-//    id: 'trans',
-//    contexts: ['selection'],
-//    onclick: translate
-//});
 
 chrome.contextMenus.create({
     type: 'normal',
